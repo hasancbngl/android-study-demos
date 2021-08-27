0564f8d40c5cecf4777df632f31376bea5c14f45
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-08-27 11:21:29 +0300

    delete petsave

--- PetSave/app/src/main/java/com/raywenderlich/android/petsave/common/domain/model/pagination/PaginatedAnimals.kt
+++
@@ -1,8 +0,0 @@
-package com.raywenderlich.android.petsave.common.domain.model.pagination
-
-import com.raywenderlich.android.petsave.common.domain.model.animal.details.AnimalWithDetails
-
-data class PaginatedAnimals(
-    val animals: List<AnimalWithDetails>,
-    val pagination: Pagination
-)
