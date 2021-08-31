b48e6cc58ec2fc76a0f5e50ce297be22e1fe8275
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-08-31 15:29:56 +0300

    create classes

---
+++ Testing/Basics/app/src/main/java/com/cobanogluhasan/testing_basics/RegistrationUtil.kt
@@ -0,0 +1,22 @@
+package com.cobanogluhasan.testing_basics
+
+object RegistrationUtil {
+
+    private val existingUsers = listOf("Peter", "Carl")
+
+    /**
+     * the input is not valid if...
+     * ...the username/password is empty
+     * ...the username is already taken
+     * ...the confirmed password is not the same as the real password
+     * ...the password contains less than 6 digits
+     */
+
+    fun validateRegistrationInput(
+        username: String,
+        password: String,
+        confirmedPassword: String
+    ): Boolean {
+        return true
+    }
+}
---
+++ Testing/Basics/app/src/test/java/com/cobanogluhasan/testing_basics/RegistrationUtilTest.kt
@@ -0,0 +1,11 @@
+package com.cobanogluhasan.testing_basics
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class RegistrationUtilTest {
+
+    @Test
+    fun `empty username return false`() {
+    }
+}
