7c7cec6ec198895edbd5d70b1b03aa6f3e801c70
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-12-03 00:25:37 +0300

    init project

---
+++ background-stuff/app/src/main/java/com/sumzed/background_stuff/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.sumzed.background_stuff
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sumzed.background_stuff.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
